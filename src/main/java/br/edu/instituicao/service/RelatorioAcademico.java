34c5e7565531b9b3b49dc12ffc8051517b5fcb5e
Antonino <antoninomenegassi@gmail.com>
2026-05-06 17:58:04 -0300

    criado relatorio acadêmico
    função de calcular a média global da instituição

--- src/main/java/br/edu/instituicao/service/RelatorioAcademico.java
+++ src/main/java/br/edu/instituicao/service/RelatorioAcademico.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.edu.instituicao.service;
 
-/**
- *
- * @author anton
- */
-public class RelatorioAcademico {
+import br.edu.instituicao.interfaces.Avaliavel;
+
+public class RelatorioAcademico { 
     
+    private double mediaglobal;
+    private int numerodealunos;
+    
+    public double getMediaglobal() {
+        return mediaglobal;
+    }
+
+    public int getNumerodealunos() {
+        return numerodealunos;
+    }
+    
+    
+    public double adicionarDados(Avaliavel objeto){
+        double somatotal = (objeto.getMediaFinal() + (mediaglobal * numerodealunos));
+        numerodealunos += 1;
+        
+        mediaglobal = somatotal/numerodealunos;
+        return mediaglobal;
+    }
 }
